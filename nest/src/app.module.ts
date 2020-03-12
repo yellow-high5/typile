757a2da12889ffb609f5efe20fd89205d6e17355
yoshinori.matsuzaki <yoshinori.matsuzaki@monocrea.co.jp>
2020-03-12 21:54:38 +0900

    [nest]create nest module

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,17 +1,7 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/user.module';
-import { join } from 'path';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      typePaths: ['./**/*.graphql'],
-      definitions: {
-        path: join(process.cwd(), 'src/graphql.ts'),
-        outputAs: 'class',
-      },
-    }),
-  ],
+  imports: [UsersModule],
 })
 export class AppModule {}
